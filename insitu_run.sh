ec1a633f754125675385228a69113c533a4dc108
wfortin136 <wjf136@uchicago.edu>
2015-02-06 20:06:21 +0000

    Updated runs and output

--- insitu_run.sh
+++ insitu_run.sh
@@ -2,14 +2,14 @@
 
 # Run batch processes for insitu_stats library
 
-
-global_size=( 4 8 16 32 64 128 256)
-local_size=( 4 8 16 32 64 128 256)
+global_size=( 4 8 16 32 64 128)
+local_size=( 4 8 16 32 64 128)
 mode=( 64 32 16 8 4 2 1 )
 
 #run_command="mpiexec"
 run_command="runjob"
-stand_input="--block $COBALT_PARTNAME --verbose=INFO :"
+LOCARGS="--block $COBALT_PARTNAME ${COBALT_CORNER:+--corner} $COBALT_CORNER ${COBALT_SHAPE:+--shape} $COBALT_SHAPE"
+stand_input="--verbose=INFO :"
 run_file="./3D_Grid"
 out_file="run_results.txt"
 
@@ -35,7 +35,7 @@ do
       for k in "${mode[@]}"
       do
         let "p= $num % $k"
-        echo "np:$num k:$k p:$p"
+        #echo "np:$num k:$k p:$p"
         if [ $p == 0 ]; then 
           per_node="-p $k"
           break
@@ -53,7 +53,7 @@ do
       #$run_command $num_proc $run_file $g_param $l_param | sudo tee -a $out_file
       #of the form: runjob --np # -p # --block $COBALT_PARTNAME --verbose=INFO : run1.exe arg1
       #from: http://www.alcf.anl.gov/user-guides/running-jobs#submitting-a-script-job
-      #echo "$run_command $num_proc $per_node $stand_input $run_file $g_param $l_param >> $out_file"
+      $run_command $LOCARGS $num_proc $per_node $stand_input $run_file $g_param $l_param >> $out_file
     fi
   done
 done
